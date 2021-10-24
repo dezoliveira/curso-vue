3ddf45793dc6a7547f01cdada77d820bc7fdbee0
Andres Oliveira <andres.oliveira@zeeng.com.br>
2021-10-24 00:10:19 -0300

    Primeira parte do Curso

---
+++ vuecli/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Componente />
+</template>
+
+<script>
+  import Componente from './components/Componente.vue'
+
+  export default {
+    name: 'App',
+    components:{
+      Componente
+    },
+  }
+</script>
---
+++ vuecli/src/components/Componente.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h1>Olá vue</h1>
+  <p>Meu nome é {{ nome }}</p>
+  <p>Sou {{profissao}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Componente",
+  data(){
+    return{
+      nome: "Andrés",
+      profissao: "Dev Front-End"
+    }
+  }
+}
+</script>
