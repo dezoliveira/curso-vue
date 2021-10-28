cbab7f1d4928ed73f63b7df068162ab35502d794
Andres Oliveira <andres.oliveira@zeeng.com.br>
2021-10-27 21:20:17 -0300

    Projeto + Form

--- makeyourburger/src/App.vue
+++ makeyourburger/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-<div>
-  <Navbar />
+  <Navbar :logo="logo_src" :alt="app_name" id="logo_url"/>
   <router-view/>
   <Footer />
-</div>
 </template>
 <script>
 import Navbar from './components/Navbar.vue'
@@ -12,10 +10,32 @@ export default {
   components:{
     Navbar,
     Footer
+  },
+  data(){
+    return{
+      logo_src: "/img/logo.png",
+      app_name: "Make your burger"
+    }
   }
 }
 </script>
-
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container{
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1{
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
